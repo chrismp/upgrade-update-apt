3becaa2e81f7147e02a9a5c96134a8c1a8113816
Chris P <chris@debian-desktop.personal.com>
2017-02-06 22:29:03 -0500

    wrapped in

--- upgrade-update-apt.sh
+++ upgrade-update-apt.sh
@@ -2,8 +2,7 @@
 
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 LOG_DIR="/media/chris/Seagate Expansion Drive1/DebianBackups/upgrade-update-apt-logs"
-LOG_FILE="$LOG_DIR/$NOW.log"
 
 mkdir -p "$LOG_DIR"
 
-nohup apt-get upgrade -y && apt-get update -y > "$LOG_FILE" 2>&1 &
+nohup sh -c 'apt-get upgrade -y && apt-get update -y' > "$LOG_DIR/$NOW.log" 2>&1 &
